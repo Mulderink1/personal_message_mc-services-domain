c0fef5d4d7a740c985e13e4980a546d1aa0954ce
Mike Mulderink <mulderink1@gmail.com>
2022-03-01 23:14:55 -0800

    Updated Package v7

--- entities.go
+++ entities.go
@@ -7,3 +7,15 @@ import (
 type Logger struct {
 	Logger *zap.Logger
 }
+
+func (l Logger) Info(message string, fields ...zap.Field) {
+	l.Logger.Info(message, fields...)
+}
+
+func (l Logger) Debug(message string, fields ...zap.Field) {
+	l.Logger.Debug(message, fields...)
+}
+
+func (l Logger) Error(message string, fields ...zap.Field) {
+	l.Logger.Error(message, fields...)
+}
